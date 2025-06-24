836e1333c9dc9e3dfc4eb1e1ca429d16595196f6
AuTeLipi <lipipgm@gmail.com>
2025-06-24 09:28:00 +0530

    24th June 2025

---
+++ src/Ex_16_StringBuffer_Builder_StringFunctions/Lab146_SB.java
@@ -0,0 +1,10 @@
+package Ex_16_StringBuffer_Builder_StringFunctions;
+
+public class Lab146_SB {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("Pramod");
+        stringBuilder.append(123);
+
+        System.out.println(stringBuilder);
+    }
+}
