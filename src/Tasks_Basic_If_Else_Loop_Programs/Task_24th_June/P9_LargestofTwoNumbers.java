994def2a36b1f1800d10e01dd4e1305e24af26a0
AuTeLipi <lipipgm@gmail.com>
2025-06-29 17:41:47 +0530

    29th June 2025

---
+++ src/Tasks_Basic_If_Else_Loop_Programs/Task_24th_June/P9_LargestofTwoNumbers.java
@@ -0,0 +1,29 @@
+package Tasks_Basic_If_Else_Loop_Programs.Task_24th_June;
+
+import java.util.Scanner;
+
+public class P9_LargestofTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter 1st Number");
+        if(!scanner.hasNextInt()) {
+            System.out.println("Enter int only!");
+            return;
+        }
+        int n1 = scanner.nextInt();
+
+        System.out.println("Enter 2nd Number");
+        if(!scanner.hasNextInt()) {
+            System.out.println("Enter int only!");
+            return;
+        }
+        int n2 = scanner.nextInt();
+
+        if(n1 > n2) {
+            System.out.println("Largest Number is "+n1);
+        } else {
+            System.out.println("Largest Number is " +n2);
+        }
+    }
+}
