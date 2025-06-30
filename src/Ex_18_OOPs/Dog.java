0b7a0201359b7996b515d8f3a3dba5670ee67ce6
AuTeLipi <lipipgm@gmail.com>
2025-06-30 08:13:13 +0530

    30th June 2025

---
+++ src/Ex_18_OOPs/Dog.java
@@ -0,0 +1,16 @@
+package Ex_18_OOPs;
+
+public class Dog {
+    //Attributes
+    String name;
+    String breed;
+    String color_eyes;
+
+
+    //Behaviours
+    void walk() {
+    }
+
+    void bark() {
+    }
+}
---
+++ src/Ex_18_OOPs/Lab166_OOPs.java
@@ -0,0 +1,10 @@
+package Ex_18_OOPs;
+
+public class Lab166_OOPs {
+    public static void main(String[] args) {
+
+        Person amit = new Person();
+
+        Person pramod = new Person();
+    }
+}
