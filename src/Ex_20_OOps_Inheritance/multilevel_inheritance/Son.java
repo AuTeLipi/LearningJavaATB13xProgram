87d8f8fd83ab4122871e8fc20e5e495db6b95ced
AuTeLipi <lipipgm@gmail.com>
2025-07-02 08:14:03 +0530

    02nd July 2025

---
+++ src/Ex_20_OOps_Inheritance/multilevel_inheritance/Son.java
@@ -0,0 +1,12 @@
+package Ex_20_OOps_Inheritance.multilevel_inheritance;
+
+public class Son extends Father{
+
+    void s(){
+        System.out.println("bhk3");
+    }
+
+    void home(){
+        System.out.println("Son-S3");
+    }
+}
