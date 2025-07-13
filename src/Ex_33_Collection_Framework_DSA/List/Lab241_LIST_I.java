c2ddce2fc5cef6c17cd8c8a7adfd9d5758d1cb8e
AuTeLipi <lipipgm@gmail.com>
2025-07-13 23:57:12 +0530

    13th July 2025

---
+++ src/Ex_33_Collection_Framework_DSA/List/Lab241_LIST_I.java
@@ -0,0 +1,17 @@
+package Ex_33_Collection_Framework_DSA.List;
+
+import java.util.ArrayList;
+
+public class Lab241_LIST_I {
+    public static void main(String[] args) {
+        //        List l = new ArrayList(); // Dynamic Diapatch
+        ArrayList l = new ArrayList(); // Dynamic Diapatch
+        l.add("hello");
+        l.add(123);
+        l.add(true);
+        l.add("Blah Blah");
+
+        System.out.println(l);
+
+    }
+}
