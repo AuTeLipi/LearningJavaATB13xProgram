f5b42fc5e0a73c435e77e1ea4bfacedb66fa6d08
AuTeLipi <lipipgm@gmail.com>
2025-07-14 22:15:55 +0530

    14th July 2025

---
+++ src/Ex_33_Collection_Framework_DSA/QUEUE/Lab260_ARRAYDeque.java
@@ -0,0 +1,12 @@
+package Ex_33_Collection_Framework_DSA.QUEUE;
+
+import java.util.*;
+
+public class Lab260_ARRAYDeque {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> deck = new ArrayDeque();
+        deck.push(15);
+        deck.push(10);
+        System.out.println(deck);
+    }
+}
